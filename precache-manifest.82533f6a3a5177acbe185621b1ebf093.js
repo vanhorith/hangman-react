25f08bb09803d12a9b6c9b36d1dce93550167f6c
vanhorith <57511379+vanhorith@users.noreply.github.com>
2019-11-07 18:06:09 -0800

    Updates

---
+++ precache-manifest.82533f6a3a5177acbe185621b1ebf093.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563d9b981b09d915801dc52ddeb78024",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "320b9b3b9f97a4886ddb",
+    "url": "/hangman-react/static/css/main.9a3d6f29.chunk.css"
+  },
+  {
+    "revision": "26e3ed6c107cdc1c8691",
+    "url": "/hangman-react/static/js/2.f985da6b.chunk.js"
+  },
+  {
+    "revision": "320b9b3b9f97a4886ddb",
+    "url": "/hangman-react/static/js/main.41133a88.chunk.js"
+  },
+  {
+    "revision": "039d4cc651c7885a37df",
+    "url": "/hangman-react/static/js/runtime-main.61eb8a72.js"
+  },
+  {
+    "revision": "c76d82d373d3da00d51a3715e4e90287",
+    "url": "/hangman-react/static/media/button.c76d82d3.png"
+  },
+  {
+    "revision": "517d75938a8d20fe86108d5b9c8551c0",
+    "url": "/hangman-react/static/media/gameLost.517d7593.png"
+  },
+  {
+    "revision": "8cadb83dff1ea9402a1339f9cad8e5d2",
+    "url": "/hangman-react/static/media/gameWon.8cadb83d.png"
+  },
+  {
+    "revision": "acdc426e6857619477ef38005945fc29",
+    "url": "/hangman-react/static/media/hang1.acdc426e.png"
+  },
+  {
+    "revision": "a6f9844bba30379c115d627d7e0c72df",
+    "url": "/hangman-react/static/media/hang2.a6f9844b.png"
+  },
+  {
+    "revision": "7c52cdf625c0d1abbd21e12ab948ad27",
+    "url": "/hangman-react/static/media/hang3.7c52cdf6.png"
+  },
+  {
+    "revision": "b873859793500647b329bd0ae1e08f5d",
+    "url": "/hangman-react/static/media/hang4.b8738597.png"
+  },
+  {
+    "revision": "b0ec4180894db228f0ecab027c07c751",
+    "url": "/hangman-react/static/media/hang5.b0ec4180.png"
+  },
+  {
+    "revision": "81ac58f2543a5f80e94bff31a08f68c0",
+    "url": "/hangman-react/static/media/hang6.81ac58f2.png"
+  },
+  {
+    "revision": "68cd83286e258ec866d77b0eebe7537c",
+    "url": "/hangman-react/static/media/hang7.68cd8328.png"
+  }
+]);
